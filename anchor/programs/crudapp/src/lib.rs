f62d7bddf30384396da31f46fe49e539e231ca33
casualprogram <datdatdat2k@gmail.com>
2025-02-20 23:58:30 -0500

    set the logic for journal entry

--- anchor/programs/crudapp/src/lib.rs
+++ anchor/programs/crudapp/src/lib.rs
@@ -7,15 +7,45 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 #[program]
 pub mod crudapp {
     use super::*;
+    // create journal entry
+    pub fn create_journal_entry(ctx: Context<CreateEntry>, title: String, ) -> Result<()>{
+      // writing logic to create journal entry 
+    }
+}
+
+
+
+#[derive(Accounts)] // derive accounts macro
+#[instruction(title: String)] // pass in the title with instruction macro
+// create journal entry struct
+pub struct CreateEntry<'info>{
+  
+  #[account(
+    init, // init macro to create account
+    seed = [title.as_bytes(), owner.key().as_ref()], // seed to create account, PDA
+    bump, // bump to create account after seed
+    payer = owner, // Payer of the account is the owner
+  )]
+  // account to create journal entry state
+  pub journal_entry: Account<'info, JournalEntryState>, 
+
+  // owner of the account is the signer
+  #[account(mut)]
+  pub owner: Signer<'info>,
+
+  // system program account
+  pub system_program: Program<'info, System>,
 
 }
 
-#[account]
-#[derive(InitSpace)]
+#[account] // account macro to create account
+#[derive(InitSpace)] // derive init space macro
+// journal entry state struct
 pub struct JournalEntryState{
-  pub owner: Pubkey,
-  #[max_len(50)]
-  pub title: String,
-  #[max_len(1000)]
-  pub message: String,
+  pub owner: Pubkey, // owner of the journal entry
+  #[max_len(50)] // max length of the title
+  pub title: String, // title of the journal entry
+  #[max_len(1000)] // max length of the message
+  pub message: String, // message of the journal entry
 }
+
